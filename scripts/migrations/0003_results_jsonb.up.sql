40610c1038961866aa0a68e22acd32ad015f5c67
Dani <dani.webdev@gmail.com>
2023-09-29 21:13:06 +0700

    add migrate cid

--- scripts/migrations/0003_results_jsonb.up.sql
+++ scripts/migrations/0003_results_jsonb.up.sql
@@ -14,4 +14,10 @@ BEGIN;
     ALTER TABLE results 
         ADD COLUMN data JSONB NOT NULL;
 
+    ALTER TABLE "public"."results" 
+        DROP CONSTRAINT "results_pkey",
+        ADD COLUMN "cid" int8 NOT NULL,
+        ADD CONSTRAINT "results_pkey" PRIMARY KEY ("id", "cid");
+
+    CREATE UNIQUE INDEX "id_idx" ON "public"."results" USING btree ("id");
 COMMIT;
